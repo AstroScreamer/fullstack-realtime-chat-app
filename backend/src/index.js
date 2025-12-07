b60c42bd82e7cb5eb76d5d8214670141e97db5bb
AstroScreamer <87512503+AstroScreamer@users.noreply.github.com>
2025-12-07 22:18:23 +0530

    updated backend/index.js, frontend/index.html

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,10 +31,11 @@ app.use('/api/groups', groupRoutes);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req, res) => {
+    app.get(/.*/, (req, res) => {
         res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
     });
 }
+
 
 server.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
