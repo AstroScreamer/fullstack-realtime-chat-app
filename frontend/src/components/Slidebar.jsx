bd5efa08e69fe55515656de1cea7b439a72d384c
AstroScreamer <87512503+AstroScreamer@users.noreply.github.com>
2025-12-08 01:30:03 +0530

    updated Slidebar.jsx

--- frontend/src/components/Slidebar.jsx
+++ frontend/src/components/Slidebar.jsx
@@ -131,7 +131,7 @@ const Slidebar = () => {
             >
               <div className="relative mx-auto lg:mx-0">
                 <img
-                  src={displayChat.profilePic || "/avatar.png"}
+                  src={displayChat.profilePic || "../assets/avatar.png"}
                   alt={displayChat?.Name || "Avatar"}
                   className="size-12 object-cover rounded-full"
                 />
